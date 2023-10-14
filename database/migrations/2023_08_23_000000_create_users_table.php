6d35449fc4cf79886565d5fe96638778fbc13010
Stanislav Kabin <ioffline@make-it.app>
2023-10-14 22:36:20 +0300

    migration file restored

---
+++ database/migrations/2023_08_23_000000_create_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /**
+         * WARNING !
+         *
+         * THIS MIGRATIONS ASSUME TO CLEAN LARAVEL INSTALLATION !
+         */
+        Schema::dropIfExists('users');
+        /**
+         * - removed 'name' field
+         * - changed bigint to uuid 'id' field
+         * - added soft deletes
+         */
+        Schema::create('users', function (Blueprint $table) {
+            // $table->id();
+            // $table->string('name')->index();
+            $table->uuid('id')->primary();
+            $table->boolean('is_banned')->index()->default(false);
+            //
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
