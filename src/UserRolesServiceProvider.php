8277925c0a7ce38c0fd4d731921be1e3c48644b8
Stanislav Kabin <ioffline@make-it.app>
2023-10-16 17:38:54 +0300

    User clases always should be published

--- src/UserRolesServiceProvider.php
+++ src/UserRolesServiceProvider.php
@@ -30,6 +30,20 @@ class UserRolesServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        //
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'user_roles');
+        $this->loadJsonTranslationsFrom(lang_path('vendor/user_roles'));
+        //
+        $this->configurePublishing();
+        $this->configureObservers();
+        $this->configurePolicies();
+    }
+
+    /**
+     * Configures a poblishes
+     */
+    protected function configurePublishing(): void
+    {
         if ($this->app->runningInConsole()) {
             $this->commands([
                 AssignUserRoleCommand::class,
@@ -45,34 +59,37 @@ class UserRolesServiceProvider extends ServiceProvider
             $this->publishes([__DIR__ . '/../configonfig.php' => config_path('user_roles.php')], 'config');
             $this->publishes([__DIR__ . '/../database/migrations' => base_path('database/migrations')], 'migrations');
             $this->publishes([__DIR__ . '/../resources/lang' => lang_path('vendor/user_roles')], 'lang');
-            $this->publishes([__DIR__ . '/NovaRoles.php' => app_path('/Nova/NovaRoles.php')], 'nova');
             $this->publishes([
                 __DIR__ . '/../stubs/User.php' => app_path('/Models/User.php'),
                 __DIR__ . '/../stubs/UserObserver.php' => app_path('/Observers/UserObserver.php'),
                 __DIR__ . '/../stubs/UserPolicy.php' => app_path('/Policies/UserPolicy.php'),
             ], 'user-model');
-        }
-        $this->registerPolicies();
-        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'user_roles');
-        $this->loadJsonTranslationsFrom(lang_path('vendor/user_roles'));
-        /**
-         * Observer
-         */
-        if (is_file(app_path('/Models/User.php')) && is_file(app_path('/Observers/UserObserver.php'))) {
-            // priority path
-            \App\Models\User::observe(\App\Observers\UserObserver::class);
-        } else {
-            \MakeIT\UserRoles\User::observe(\MakeIT\UserRoles\UserObserver::class);
-        }
-        /**
-         * Policy
-         */
-        Gate::policy(Role::class, RolePolicy::class);
-        if (is_file(app_path('/Policies/UserPolicy.php'))) {
-            Gate::policy(\App\Models\User::class, \App\Policies\UserPolicy::class);
-        } else {
-            Gate::policy(\MakeIT\UserRoles\User::class, \MakeIT\UserRoles\UserPolicy::class);
+            // for Laravel Nova
+            $this->publishes([__DIR__ . '/NovaRoles.php' => app_path('/Nova/NovaRoles.php')], 'nova');
         }
     }
 
+    /**
+     * Configure Policies
+     */
+    protected function configurePolicies(): void
+    {
+        if (config('user_roles.policiesToRegister', [])) {
+            $this->policies = config('user_roles.policiesToRegister');
+            $this->registerPolicies();
+        }
+    }
+
+    /**
+     * Configure Observers
+     */
+    protected function configureObservers(): void
+    {
+        foreach (config('user_roles.observersToRegister') as $Model => $Observer) {
+            if (class_exists($Model) && class_exists($Observer)) {
+                /** @noinspection PhpUndefinedMethodInspection */
+                $Model::observe($Observer);
+            }
+        }
+    }
 }
--- src/config.php
+++ src/config.php
@@ -2,4 +2,10 @@
 
 return [
     'default_role' => 'user',
+    'observersToRegister' => [
+        \App\Models\User::class => \App\Observers\UserObserver::class,
+    ],
+    'policiesToRegister' => [
+        \App\Models\User::class => \App\Policies\UserPolicy::class,
+    ],
 ];
